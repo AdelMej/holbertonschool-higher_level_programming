e132934157a89b896b098a12290d4a9b5a21ae52
AdelMej <adelmejrissi@gmail.com>
2025-09-01 15:29:55 +0200

    fix: fixed pystyle added code for simple operations like adding and power also added fizzbuss function and last digit one

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -2,4 +2,11 @@
 import random
 number = random.randint(-10000, 10000)
 lastdigit = abs(number) % 10
-print(f"Last digit of {number} is {lastdigit} and " + ("is greater than 5" if lastdigit > 5 else "and is zero" if lastdigit == 0 else "is less than 6 and not 0"))
+print(
+    f"Last digit of {number} is {lastdigit} and " +
+    (
+        "is greater than 5" if lastdigit > 5
+        else "and is zero" if lastdigit == 0
+        else "is less than 6 and not 0"
+    )
+)
---
+++ python-if_else_loops_functions/10-add.py
@@ -0,0 +1,5 @@
+#!/bin/python3
+
+
+def add(a, b):
+    return a + b
