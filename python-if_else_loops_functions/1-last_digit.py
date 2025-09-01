e9f1690d577231f6c22b0ae39c36fc880f6a7c44
AdelMej <adelmejrissi@gmail.com>
2025-09-01 15:36:17 +0200

    fix: removed and to and is zero facepalm

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -6,7 +6,7 @@ print(
     f"Last digit of {number} is {lastdigit} and " +
     (
         "is greater than 5" if lastdigit > 5
-        else "and is zero" if lastdigit == 0
+        else "is zero" if lastdigit == 0
         else "is less than 6 and not 0"
     )
 )
