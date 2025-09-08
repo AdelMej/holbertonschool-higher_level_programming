4f786545959223c545f841eaa4c2c4e7a7cb5fcc
AdelMej <adelmejrissi@gmail.com>
2025-09-08 15:35:10 +0200

    feat: added a function that only sums one occurence of each integers in a list

---
+++ python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    occurence = []
+    sum = 0
+    for i in my_list:
+        if i not in occurence:
+            occurence.append(i)
+            sum += i
+
+    return sum
