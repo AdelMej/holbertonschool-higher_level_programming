ff3b0481c32b780cdf262849f69f91ac51d2f29a
AdelMej <adelmejrissi@gmail.com>
2025-09-09 16:40:49 +0200

    feat: added an adding function and it's tests

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""add function utility.
+
+Provides an utility function for adding numbers
+"""
+
+
+def add_integer(a, b=98):
+    """add 2 integers together.
+
+    Args:
+        a (int, float): a value to add to b
+        b (int, float): a value to add to a
+
+    Returns:
+        int: The result of a + b
+
+    Raises:
+        TypeError: if a or b is not a float or an integer
+
+    Example:
+        >>> add_integer(2, 2)
+        4
+    """
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+
+    if type(a) is float:
+        a = int(a)
+    if type(b) is float:
+        b = int(b)
+
+    return a + b
