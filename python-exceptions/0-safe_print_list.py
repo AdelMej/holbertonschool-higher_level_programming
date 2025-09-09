1586e41e589ded081897e4db5bb7e4a9d19b0634
AdelMej <adelmejrissi@gmail.com>
2025-09-09 14:01:09 +0200

    fix: changed where the try catch is

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     nb_elem = 0
-    try:
-        for i in range(x):
-            print(my_list[i], end="" if i < x - 1 else "\n")
+    for i in range(x):
+        try:
+            print(my_list[i], end="")
             nb_elem += 1
-    except IndexError:
-        print()
-        return nb_elem
-
+        except IndexError:
+            break
+    print()
     return nb_elem
