9b61450649086670760270fbfe8774e25899b703
AdelMej <adelmejrissi@gmail.com>
2025-09-09 14:15:18 +0200

    fix: order of operations

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -7,11 +7,10 @@ def list_division(my_list_1, my_list_2, list_length):
         except ZeroDivisionError:
             new_list.append(0)
             print("division by 0")
-        except TypeError:
-            new_list.append(0)
-            print("wrong type")
         except IndexError:
             new_list.append(0)
             print("out of range")
-
+        except TypeError:
+            new_list.append(0)
+            print("wrong type")
     return new_list
