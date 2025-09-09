8b6146b5790011603dcb6b8c97e67f0c72a2b95d
AdelMej <adelmejrissi@gmail.com>
2025-09-09 13:08:51 +0200

    feat: added a script that return the number of element printed even if x is bigger than the list size

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    nb_elem = 0
+    try:
+        for i in range(x):
+            print(my_list[i], end="" if i < x - 1 else "\n")
+            nb_elem += 1
+    except IndexError:
+        return nb_elem
+
+    return nb_elem
