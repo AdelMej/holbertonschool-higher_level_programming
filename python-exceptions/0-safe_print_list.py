4f399991ca301e3605cae02a8277792170b35bcc
AdelMej <adelmejrissi@gmail.com>
2025-09-09 13:56:11 +0200

    feat: print a newline on exception

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -6,6 +6,7 @@ def safe_print_list(my_list=[], x=0):
             print(my_list[i], end="" if i < x - 1 else "\n")
             nb_elem += 1
     except IndexError:
+        print()
         return nb_elem
 
     return nb_elem
