703c6e12881decdbd3c34760f4c27e8ebd0301ca
AdelMej <adelmejrissi@gmail.com>
2025-09-09 12:41:16 +0200

    fix: return none instead of an empty string if no key, values are found

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -3,10 +3,11 @@ def best_score(a_dictionary):
     if a_dictionary is None:
         return None
 
-    maxKey = ""
+    maxKey = None
     maxVal = 0
     for key, value in a_dictionary.items():
         if value > maxVal:
             maxVal = value
             maxKey = key
+
     return maxKey
