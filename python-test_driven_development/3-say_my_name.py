1e36016db03c2ae397b9b26d971bbce2f8d30bf7
AdelMej <adelmejrissi@gmail.com>
2025-09-10 14:04:18 +0200

    feat: added a function that prints a square also added it's tests and updated say my name function

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -18,9 +18,9 @@ def say_my_name(first_name, last_name=""):
         TypeError: first_name should be a string
         TypeError: last_name should be a string
     """
-    if not isinstance(first_name, str) :
+    if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
 
-    print("My name is {:s}{:s}".format(first_name, " " + last_name if last_name else ""))
+    print("My name is {:s} {:s}".format(first_name, last_name))
---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3-config
+"""a library to print a square
+
+can print a square depending on a given integer
+should be type safe guarded
+"""
+
+
+def print_square(size):
+    """Prints a square.
+    
+    Args:
+        size(int): the size of the square
+
+    Returns:
+        None
+
+    Raises:
+        TypeError: if size is not an integer
+        ValueError: if size is less than 0
+    """
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    for _ in range(size):
+        for _ in range(size):
+            print("#", end="")
+        print()
