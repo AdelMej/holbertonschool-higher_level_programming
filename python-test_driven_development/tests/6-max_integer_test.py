5a7aa57a73750e203640e20ddb40c7110be5505f
AdelMej <adelmejrissi@gmail.com>
2025-09-10 16:51:07 +0200

    feat: added a unit test for max_integer

---
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,23 @@
+"""Unittest for max_integer([..])
+"""
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+class TestMaxInteger(unittest.TestCase):
+    """Test suite for max_integer
+
+    """
+    def test_max_integer(self):
+        self.assertEqual(max_integer([1, 10, 334]), 334)
+
+    def test_max_integer_negative(self):
+        self.assertEqual(max_integer([-1, -46, -2]), -1)
+
+    def test_max_integer_empty_list(self):
+        self.assertEqual(max_integer([]), None)
+
+    def test_max_integer_string_in_list(self):
+        self.assertEqual(max_integer([2, "2", 3]), 3)
+
+if __name__ == '__main__':
+    unittest.main()
