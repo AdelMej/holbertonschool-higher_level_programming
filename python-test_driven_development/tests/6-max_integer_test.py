135f500f119b0b7b66c0d7d48bc326c7159eaf33
AdelMej <adelmejrissi@gmail.com>
2025-09-10 17:00:55 +0200

    feat: added test for float and single list element

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -17,6 +17,13 @@ class TestMaxInteger(unittest.TestCase):
 
     def test_max_integer_empty_list(self):
         self.assertEqual(max_integer([]), None)
+    
+    def test_max_integer_single_elem(self):
+        self.assertEqual(max_integer([4]), 4)
+
+    def test_max_integer_float(self):
+        self.assertEqual(max_integer([1, 1.2, 2.3]), 2.3)
+
 
 if __name__ == '__main__':
     unittest.main()
