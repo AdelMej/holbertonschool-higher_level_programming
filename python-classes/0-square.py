48dd7e9d2f7d0731800594570b4dfeb13762bfaa
AdelMej <adelmejrissi@gmail.com>
2025-09-15 09:31:03 +0200

    feat: added an empty square class

---
+++ python-classes/0-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""square class file
+
+empty class for now
+"""
+class square():
+    """square class for square operations
+
+    does nothing
+
+    Attributes:
+        no Attributes
+    """
+    pass
