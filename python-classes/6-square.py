4a93b296697a310687d763b5cf0d048478a6253b
AdelMej <adelmejrissi@gmail.com>
2025-09-15 11:35:34 +0200

    feat: re added checks in the constructor

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -27,9 +27,17 @@ class Square():
         """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
-
         if size < 0:
             raise ValueError("size must be >= 0")
+
+        if (
+            not isinstance(position, tuple)
+            or len(position) != 2
+            or all(isinstance(i, int) and i <= 0 for i in position)
+        ):
+            raise TypeError(
+                "position must be a tuple of 2 positive integers"
+            )
 
         self.__size = size
         self.__position = position
