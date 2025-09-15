ca058e8838975b66f198be53e62f61c9efec6a47
AdelMej <adelmejrissi@gmail.com>
2025-09-15 13:17:00 +0200

    feat: moved files into their directory

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Rectangle class file
+
+contains an empty recangle class
+"""
+
+
+class Rectangle:
+    """Rectangle class to do basic rectangle operations
+
+        does nothing
+    """
+    pass
