b88b6da3c85007ad21bf0ad73805d0abea52d6b3
AdelMej <adelmejrissi@gmail.com>
2025-09-22 12:12:02 +0200

    fix: type

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -13,6 +13,6 @@ class Square(Rectangle):
         Args:
             size(int): the square size
         """
-        super().integer_validator("szie", size)
+        super().integer_validator("size", size)
         self.__size = size
         super().__init__(self.__size, self.__size)
