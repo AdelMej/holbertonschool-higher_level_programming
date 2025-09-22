40795fdd114f8a59b6a1f98ea9eef5458d26ac5d
AdelMej <adelmejrissi@gmail.com>
2025-09-22 16:20:39 +0200

    feat: added a counted iterator class

---
+++ python-abc/task_03_countediterator.py
@@ -0,0 +1,23 @@
+"""
+File containing a class for a Counted Iterator
+"""
+
+
+class CountedIterator:
+    """
+    Class extending iterator behavior
+    """
+    def __init__(self, obj):
+        """CountedIterator constructor"""
+        self.__counter = 0
+        self.__iterator = iter(obj)
+
+    def get_count(self):
+        """return current iteration"""
+        return self.__counter
+
+    def __next__(self):
+        """rewrite next to increment counter"""
+        res = next(self.__iterator)
+        self.__counter += 1
+        return res
