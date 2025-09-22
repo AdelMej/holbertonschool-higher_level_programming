23108205d1a6c5b7f54e893a4166480890f10012
AdelMej <adelmejrissi@gmail.com>
2025-09-22 12:05:48 +0200

    feat: added docstring

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -6,4 +6,7 @@ an object is a sublclass of a class return false otherwise
 
 
 def inherits_from(obj, a_class):
+    """ a function that returns if an obj
+    is an instance of a class that ineherited from a specific class
+    """
     return issubclass(type(obj), a_class) and type(obj) is not a_class
