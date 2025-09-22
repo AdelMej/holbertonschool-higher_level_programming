fd28dc70df499360f9384b075c1202aa05a6d6f1
AdelMej <adelmejrissi@gmail.com>
2025-09-22 15:14:59 +0200

    fix: removed type hints

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -8,27 +8,27 @@ from math import pi
 class Shape(ABC):
     """ abstract class defining a shape """
     @abstractmethod
-    def area(self) -> float:
-        """ abstrac method for the area of a shape """
+    def area(self):
+        """ abstract method for the area of a shape """
         pass
 
     @abstractmethod
-    def perimeter(self) -> float:
+    def perimeter(self):
         """ abstract method for the perimeter of a shape"""
         pass
 
 
 class Circle(Shape):
     """ a class representing a circle """
-    def __init__(self, radius) -> None:
+    def __init__(self, radius):
         """ Circle constructor """
         self.radius = radius
 
-    def area(self) -> float:
+    def area(self):  # type:ignore
         """ a method that return the area of a circle"""
         return pi * self.radius**2
 
-    def perimeter(self) -> float:
+    def perimeter(self):  # type: ignore
         """ a method that return the perimeter of a circle """
         return 2 * pi * self.radius
 
@@ -40,11 +40,11 @@ class Rectangle(Shape):
         self.width = width
         self.height = height
 
-    def area(self) -> float:
+    def area(self):  # type: ignore
         """ a method returning the area of a rectangle """
         return self.width * self.height
 
-    def perimeter(self) -> float:
+    def perimeter(self):  # type: ignore
         """ a method returning the perimeter of a rectangle """
         return 2 * (self.width + self.height)
 
