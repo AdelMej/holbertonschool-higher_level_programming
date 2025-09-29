64bbc4beb6e55b4c72d3d1f1a9deda636b6a20e6
AdelMej <adelmejrissi@gmail.com>
2025-09-29 16:37:30 +0200

    fix: removed \n of print

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -7,4 +7,4 @@ file containing code to read a file
 def read_file(filename=""):
     """Function to read a file"""
     with open(filename, "r", encoding="utf-8") as f:
-        print(f.read())
+        print(f.read(), end="")
