884d31efab848733922fae871efbf6672b2a35b9
AdelMej <adelmejrissi@gmail.com>
2025-09-29 16:36:15 +0200

    feat: added read mode to open

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -6,5 +6,5 @@ file containing code to read a file
 
 def read_file(filename=""):
     """Function to read a file"""
-    with open(filename, encoding="utf-8") as f:
+    with open(filename, "r", encoding="utf-8") as f:
         print(f.read())
