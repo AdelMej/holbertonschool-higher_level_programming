24b43c71070f04395c19eaf0b6f7ef8ee538a7f7
AdelMej <adelmejrissi@gmail.com>
2025-09-29 16:42:19 +0200

    fix: changed the order of attribute storage cause the checker sucks

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -8,8 +8,8 @@ class Student:
     """Student Class"""
     def __init__(self, first_name, last_name, age):
         """Student constructor"""
-        self.first_name = first_name
         self.last_name = last_name
+        self.first_name = first_name
         self.age = age
 
     def to_json(self, attrs=None):
