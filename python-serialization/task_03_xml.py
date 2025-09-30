9aac43bfe29be4218f3d5eee285921e3d1ad603c
AdelMej <adelmejrissi@gmail.com>
2025-09-30 16:42:03 +0200

    feat: added a serialization and deserialization for xml files

--- python-serialization/task_03_xml.py
+++ python-serialization/task_03_xml.py
@@ -8,8 +8,19 @@ def serialize_to_xml(dictionary, filename):
     """Function that serialize a dictionary to an xml"""
     root = ET.Element("data")
     for key, value in dictionary.items():
-        ET.SubElement(root, key).text = value
+        ET.SubElement(root, key).text = str(value)
 
     # write in a file
     tree = ET.ElementTree(root)
-    tree.write(filename)
+    tree.write(filename, encoding="utf-8", xml_declaration=True)
+
+
+def deserialize_from_xml(filename):
+    """Function to deserialize from an xml file"""
+    tree = ET.parse(filename)
+    root = tree.getroot()
+    res = {}
+    for child in root:
+        res[child.tag] = child.text
+
+    return res
