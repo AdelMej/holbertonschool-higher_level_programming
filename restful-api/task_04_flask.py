531665090333fc70a823b34a2f9de93cf27f33c2
AdelMej <adelmejrissi@gmail.com>
2025-10-07 14:45:50 +0200

    feat: added flask task with route dynamic routing and error handling

---
+++ restful-api/task_04_flask.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+users = {"jane": {"username": "jane", "name": "Jane", "age": 28, "city": "Los Angeles"}, "john": {"username": "john", "name": "John", "age": 30, "city": "New York"}}
+
+
+@app.route("/")
+def home():
+    return "Welcome to the Flask API!", 200
+
+
+@app.route("/data")
+def data():
+    return jsonify([key for key in users]), 200
+
+
+@app.route("/status")
+def status():
+    return "OK", 200
+
+
+@app.route("/users/<username>")
+def show_user(username):
+    user = users.get(username)
+    if user:
+        return jsonify(user), 200
+    else:
+        return jsonify({"error": "User not found"}), 404
+
+
+@app.route("/add_user", methods=["POST"])
+def add_user():
+    data = request.get_json()
+    if not data:
+        return "invalid data", 400
+
+    if "username" not in data:
+        return jsonify({"error": "Username is required"}), 400
+
+    users[data["username"]] = {
+        "username": data["username"],
+        "name": data["name"],
+        "age": data["age"],
+        "city": data["city"]
+    }
+    return jsonify({"message": "User added", "user": data}), 201
+
+
+if __name__ == "__main__":
+    app.run()
