df5342699f205f843e3701b663b9758ade910120
AdelMej <adelmejrissi@gmail.com>
2025-10-07 10:58:05 +0200

    fix: readded header files

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -12,9 +12,13 @@ class Server(BaseHTTPRequestHandler):
     def do_GET(self):
         if self.path == '/':
             self.send_response(200)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
             self.wfile.write(b"Hello, this is a simple API!")
         elif self.path == '/data':
             self.send_response(200)
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
             data = {"name": "John", "age": 30, "city": "New York"}
             self.wfile.write(json.dumps(data).encode(encoding="utf-8"))
         elif self.path == '/status':
