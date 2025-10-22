6a1c00170e404de013a9eed5947bede650c288bf
Adel <adelmejrissi@gmail.com>
2025-10-22 14:00:08 +0200

    fixed typo causing the query to not work

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,4 @@
 -- script that create a database and a user with select privilege
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-CREATE USER 'user_0d_2'@'localhost' INDENTIFIED BY 'user_0d_2_pwd';
+CREATE USER 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
