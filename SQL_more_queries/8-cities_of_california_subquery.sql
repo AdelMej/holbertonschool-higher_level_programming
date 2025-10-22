eca524d4537d0a553bd1f2abdac089eb2eed9ea4
Adel <adelmejrissi@gmail.com>
2025-10-22 16:58:06 +0200

    fix: query id and name instead of everything

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,7 +1,7 @@
 -- select all the cities from California id 1
-SELECT *
+SELECT id, name
 FROM cities
 WHERE state_id = (
-SELECT id FROM states WHERE name = 'California'
+	SELECT id FROM states WHERE name = 'California'
 )
 ORDER BY id ASC;
