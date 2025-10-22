4621babd69595888fdbf48fc31c7356c09fcd065
Adel <adelmejrissi@gmail.com>
2025-10-22 17:30:52 +0200

    feat: added a script that filter out all of the tv hsows with no genre

---
+++ SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,6 @@
+-- select all tv shows even if they don't have an id
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_show_genres.genre_id IS NULL
+ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
