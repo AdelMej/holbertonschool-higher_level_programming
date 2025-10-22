d14f9846762708b0e769c8ffe1ebdf833fbbb04b
Adel <adelmejrissi@gmail.com>
2025-10-22 17:26:28 +0200

    feat: added a script that select all tv shows including the ones with no genre

---
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,5 @@
+-- select all tv shows even if they don't have an id
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
