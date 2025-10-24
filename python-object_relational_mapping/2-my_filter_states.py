e65a13bf8acec4feabd8809cfc26f014311982b6
AdelMej <adelmejrissi@gmail.com>
2025-10-24 10:17:22 +0200

    fix: change = to LIKE BINARY to be sure the argument match perfectly

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -24,7 +24,7 @@ if __name__ == "__main__":
     cursor = db.cursor()
     cursor.execute(
                    """SELECT * FROM states
-                   WHERE states.name = '{}'
+                   WHERE states.name LIKE BINARY '{}'
                    ORDER BY states.id ASC""".format(name)
                    )
     for query in cursor.fetchall():
