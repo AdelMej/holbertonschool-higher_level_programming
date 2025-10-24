10e3ff8e265bb1996f05751cdaafbe7f8541a4a2
AdelMej <adelmejrissi@gmail.com>
2025-10-24 09:42:28 +0200

    fix: forgot to order the query added order by

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -20,7 +20,7 @@ if __name__ == "__main__":
     )
 
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states WHERE states.name LIKE 'N%'")
+    cursor.execute("SELECT * FROM states WHERE states.name LIKE 'N%' ORDER BY states.id ASC")
     for query in cursor.fetchall():
         print(query)
 
