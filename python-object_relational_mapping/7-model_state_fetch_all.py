7cbe28349817f7ac9d8f1b39066c0a2882fb0aa4
AdelMej <adelmejrissi@gmail.com>
2025-10-24 13:36:27 +0200

    feat: added a script that query all the states in the db

---
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,22 @@
+"""
+File containing a script for Selecting all states
+From hbtn_0e_6_usa
+"""
+from sys import argv
+from sqlalchemy import (create_engine)
+from model_state import Base, State
+from sqlalchemy.orm import Session, sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost/{}".format(argv[1], argv[2], argv[3])
+    )
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    states = session.query(State).all()
+    for state in states:
+        print("{}: {}".format(state.id, state.name))
+
+    session.close()
