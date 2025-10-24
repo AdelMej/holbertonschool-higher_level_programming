e1c5610402f4c0ec0f0c22439abcd5dca9feb7a5
AdelMej <adelmejrissi@gmail.com>
2025-10-24 10:02:35 +0200

    fix: added key word Binary to avoid fetching lowercase values

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -23,7 +23,7 @@ if __name__ == "__main__":
     cursor = db.cursor()
     cursor.execute(
                    "SELECT * FROM states "
-                   "WHERE states.name LIKE 'N%' "
+                   "WHERE states.name LIKE BINARY 'N%' "
                    "ORDER BY states.id ASC"
                    )
     for query in cursor.fetchall():
