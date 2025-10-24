074882c109ba43542f95d4c9054e639eea386f87
AdelMej <adelmejrissi@gmail.com>
2025-10-24 13:44:21 +0200

    fix: forgot all statement and added poolèprèping = true

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -9,13 +9,18 @@ from sqlalchemy.orm import Session, sessionmaker
 
 if __name__ == "__main__":
     engine = create_engine(
-        "mysql+mysqldb://{}:{}@localhost:3306/{}".format(argv[1], argv[2], argv[3])
+        "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+            argv[1],
+            argv[2],
+            argv[3]
+        ),
+        pool_pre_ping=True
     )
 
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    states = session.query(State).order_by(State.id.asc())
+    states = session.query(State).order_by(State.id.asc()).all()
     for state in states:
         print("{}: {}".format(state.id, state.name))
 
