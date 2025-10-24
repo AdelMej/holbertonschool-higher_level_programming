73ccbf0724077ab0272546ebd668df286dbff813
AdelMej <adelmejrissi@gmail.com>
2025-10-24 09:38:08 +0200

    feat: changed gitignore name and created a python script for listing all states from a db

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+File containing a script for connection to a database
+"""
+from sys import argv
+import MySQLdb
+
+if __name__ == "__main__":
+    mysql_username = argv[1]
+    mysql_password = argv[2]
+    mysql_database = argv[3]
+
+    db = MySQLdb.connect(
+        host="localhost",
+        user=mysql_username,
+        passwd=mysql_password,
+        db=mysql_database,
+        charset="utf8mb4"
+    )
+
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states")
+    for query in cursor.fetchall():
+        print(query)
+
+    cursor.close()
+    db.close()
