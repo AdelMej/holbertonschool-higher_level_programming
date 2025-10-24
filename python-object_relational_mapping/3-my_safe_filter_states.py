3ec9c852b787b9ada2cf6f45e4642c99868f147f
AdelMej <adelmejrissi@gmail.com>
2025-10-24 10:30:59 +0200

    feat: added a script that query a name given in parameter this version is safe from sql injection

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+Lists all states with a name given in arguments safe from injections
+from the database hbtn_0e_0_usa.
+"""
+from sys import argv
+import MySQLdb
+
+if __name__ == "__main__":
+    mysql_username = argv[1]
+    mysql_password = argv[2]
+    mysql_database = argv[3]
+    name = argv[4]
+
+    db = MySQLdb.connect(
+        host="localhost",
+        user=mysql_username,
+        passwd=mysql_password,
+        db=mysql_database,
+        port=3306,
+        charset="utf8mb4"
+    )
+
+    cursor = db.cursor()
+    cursor.execute(
+                   """SELECT * FROM states
+                   WHERE BINARY name = %s
+                   ORDER BY states.id ASC""",
+                   (name,)
+                   )
+    for query in cursor.fetchall():
+        print(query)
+
+    cursor.close()
+    db.close()
