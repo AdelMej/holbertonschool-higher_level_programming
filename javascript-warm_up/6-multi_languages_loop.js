2cf57ce872891ee6272bce18826022a3325632cf
AdelMej <adelmejrissi@gmail.com>
2025-11-07 10:56:10 +0100

    fix: type on javascript

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,3 +1,3 @@
 #!/usr/bin/node
-const toPrint = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const toPrint = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (let i = 0; i < toPrint.length; i++) console.log(toPrint[i]);
