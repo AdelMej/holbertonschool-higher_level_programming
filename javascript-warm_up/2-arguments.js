ee4116ecf9601407da504d8ea42ee5311eb006be
AdelMej <adelmejrissi@gmail.com>
2025-11-07 10:20:19 +0100

    fix: missing condition for multiple arguments

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,4 +1,5 @@
 #!/usr/bin/node
 const { argv } = require('node:process');
 if (argv.length <= 2) console.log('No argument');
-else console.log('Argument found');
+else if (argv.length == 3) console.log('Argument found');
+else console.log('Arguments found')
