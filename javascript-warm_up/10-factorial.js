7758f3f8f57afc99b248423931b7eda1063d9854
AdelMej <adelmejrissi@gmail.com>
2025-11-07 11:40:17 +0100

    feat: added a script that does factorial recursively and return the result

---
+++ javascript-warm_up/10-factorial.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const { argv } = require('node:process');
+
+function factorial (num) {
+  if (num === 0) {
+    return 1;
+  }
+  return num * factorial(num - 1);
+}
+if (isNaN(argv[2])) {
+  console.log(1);
+} else {
+  console.log(factorial(argv[2]));
+}
