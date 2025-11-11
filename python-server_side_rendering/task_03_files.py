ddb6a75f9f257ea0cd558ab33c21f95f67679339
Adel <adelmejrissi@gmail.com>
2025-11-11 15:33:35 +0100

    feat: added code for rendering csv or json files also output error codes if necessary handle single ids too

--- python-server_side_rendering/task_03_files.py
+++ python-server_side_rendering/task_03_files.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import json
+import csv
 
 app = Flask(__name__)
 
@@ -25,6 +26,41 @@ def items():
         data = json.load(f)
 
     return render_template('items.html', items=data["items"])
+
+
+@app.route('/products')
+def products():
+    source = request.args.get('source')
+    opt_id = request.args.get('id')
+    output = None
+
+    if source == "csv":
+        try:
+            with open("products.csv", "r") as f:
+                reader = csv.DictReader(f)
+                output = [items for items in reader]
+        except Exception:
+            pass
+
+    if source == "json":
+        try:
+            with open("products.json", "r") as f:
+                output = json.load(f)
+        except Exception:
+            pass
+
+    if not output:
+        return render_template('product_display.html', error='Wrong source')
+
+    if opt_id:
+        output = next((p for p in output if str(p.get("id")) == str(opt_id)), None)
+
+        if not output:
+            return render_template('product_display.html', error='Product not found')
+
+        return render_template('product_display.html', items=[output])
+
+    return render_template('product_display.html', items=output)
 
 
 if __name__ == '__main__':
