01a05ca536e20837272d4052c56827404ef39d2f
Adel <adelmejrissi@gmail.com>
2025-11-11 16:12:37 +0100

    feat: added a source that handles sqlite fetching and rendering also work with previous features

---
+++ python-server_side_rendering/task_04_db.py
@@ -0,0 +1,75 @@
+from flask import Flask, render_template, request
+import json
+import csv
+import sqlite3
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+
+@app.route('/items')
+def items():
+    with open("items.json", "r") as f:
+        data = json.load(f)
+
+    return render_template('items.html', items=data["items"])
+
+
+@app.route('/products')
+def products():
+    source = request.args.get('source')
+    opt_id = request.args.get('id')
+    output = None
+
+    if source == "csv":
+        try:
+            with open("products.csv", "r") as f:
+                reader = csv.DictReader(f)
+                output = [items for items in reader]
+        except Exception:
+            pass
+
+    if source == "json":
+        try:
+            with open("products.json", "r") as f:
+                output = json.load(f)
+        except Exception:
+            pass
+
+    if source == "sql":
+        with sqlite3.connect("products.db") as conn:
+            conn.row_factory = sqlite3.Row
+            cursor = conn.cursor()
+            data = cursor.execute('SELECT * FROM Products').fetchall()
+            output = [dict(row) for row in data]
+
+    if not output:
+        return render_template('product_display.html', error='Wrong source')
+
+    if opt_id:
+        output = next((p for p in output if str(p.get("id")) == str(opt_id)), None)
+
+        if not output:
+            return render_template('product_display.html', error='Product not found')
+
+        return render_template('product_display.html', items=[output])
+
+    return render_template('product_display.html', items=output)
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
